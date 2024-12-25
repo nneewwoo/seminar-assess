f11961650ebe04195587632ef333f7e02295e1d9
Owen Garbosa <garbosa.owen@student.capsu.edu.ph>
2024-12-25 12:53:56 +0800

    add logger

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,21 +1,55 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
+
 import { db } from '@seminar-assess/db'
 import { users } from '@seminar-assess/db/schema'
-import { eq } from '@seminar-assess/db/drizzle'
-import Bun from 'bun'
 
 const app = new Hono()
 
-app.get('/', async ({ json }) => {
-  const result = await db
-    .select()
-    .from(users)
-    .where(eq(users.email, 'email@email.com'))
+app.use('*', logger())
+
+app.get('/health', ({ json }) => {
+  return json({
+    status: 'OK',
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString()
+  })
+})
+
+app.get('/testdb', async ({ json }) => {
+  const result = await db.select({ id: users.id }).from(users)
 
   return json(result)
 })
 
-Bun.serve({
-  port: Bun.env.PORT || 3000,
-  fetch: app.fetch
+app.onError((err, c) => {
+  console.error(`[Error] ${err.message}`)
+  console.error(err.stack)
+
+  if (process.env.NODE_ENV === 'production') {
+    return c.json(
+      {
+        error: {
+          message: 'Internal Server Error',
+          id: crypto.randomUUID()
+        }
+      },
+      500
+    )
+  }
+
+  return c.json(
+    {
+      error: {
+        message: err.message,
+        stack: err.stack
+      }
+    },
+    500
+  )
 })
+
+export default {
+  port: Number(process.env.PORT) || 3000,
+  fetch: app.fetch
+}
